e2461b656ea77ae968186cb09ebc967e2fdeb86c
Bolot <kemelovb98@gmail.com>
2026-02-26 13:42:16 +0600

    Add Flask API with Docker

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-print("Hello, this is my first Docker project")
-print("I am learning DevOps")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps API is running"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
